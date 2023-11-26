d392254831f78ec4ed769a12a309be049e99f446
Syed9700 <56927230+Syed9700@users.noreply.github.com>
2023-11-26 23:39:07 +1100

    Update jfPush.groovy

--- vars/jfPush.groovy
+++ vars/jfPush.groovy
@@ -1,1 +1,4 @@
-
+def call() {
+  sh 'cd target' 
+      'curl -X PUT -u 'admin:Admin12345' -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://54.153.8.29:8082/artifactory/example-repo-local/'
+}
