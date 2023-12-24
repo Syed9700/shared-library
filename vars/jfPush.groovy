acaaf2726e4bb49fc4da43ad674bf37bfcc17246
Arif <56927230+Syed9700@users.noreply.github.com>
2023-12-24 13:37:21 +1100

    Update jfPush.groovy

--- vars/jfPush.groovy
+++ vars/jfPush.groovy
@@ -1,4 +1,4 @@
 def call() {
   sh 'cd target' 
-      'curl -X PUT -u 'admin:Admin1234' -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://54.153.8.29:8082/artifactory/example-repo-local/'
+      'curl -X PUT -u admin:Admin1234 -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://54.153.8.29:8082/artifactory/example-repo-local/'
 }
